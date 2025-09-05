0348fa67225f40aca2f74366c686edd58bbbe69d
saurav kumar <sauravkumar8887252285@gmail.com>
2025-09-06 01:24:50 +0530

    Added .gitignore and removed node_modules + .env from git tracking

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import jwt from 'jsonwebtoken'
-const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs'
 import validator from 'validator'
 import 'dotenv/config'
 
